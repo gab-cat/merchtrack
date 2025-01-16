3e20f4e69feeaed1a5af43d7685dd46715cc65cf
Reibanns <reivenlee@gmail.com>
2025-01-16 19:11:34 +0800

    feat(header-implementation): add header and tailwind config

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,7 @@ export default async function Home() {
   return (
     <div>
       <HeaderLP />
-      <div className="text-4xl font-bold text-center">
-        
+      <div className="text-4xl font-bold text-center ">
         Hello Worlds!
       </div>
     </div>
