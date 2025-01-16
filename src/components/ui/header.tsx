480fc72c2d42135028ab33f8a0a652655665b0fd
gab-cat <catimbanggabriel@gmail.com>
2025-01-17 01:16:08 +0800

    style: adjusted styling of the header

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -31,24 +31,24 @@ const HeaderLP = () => {
   ];
   return (
     <nav className='border-b'>
-      <div className="max-w-screen-lg flex flex-row flex-wrap mx-auto items-center justify-between p-2">
+      <div className="max-w-screen-lg flex flex-row flex-wrap mx-auto items-center justify-between py-4 p-2">
         <Link href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
           <Image
             src="/img/merch-track-logo.png"
-            width={64}
-            height={64}
+            width={40}
+            height={40}
             alt="MerchTrack Logo"
           />
           <span className="self-center text-2xl font-semibold whitespace-nowrap text-neutral-8 dark:text-neutral-1">MerchTrack</span>
         </Link>
         <div className="flex md:order-2 space-x-3">
           <SignInButton mode='modal'>
-            <button className="text-primary bg-neutral-1  focus:ring-4 focus:outline-none focus:ring-accent-7 font-medium rounded-lg text-sm px-4 py-2 text-center hidden md:flex w-max outline outline-1 outline-primary hover:opacity-70">Sign In</button>
+            <button type='button' className="text-primary bg-neutral-1  focus:ring-4 focus:outline-none focus:ring-accent-7 font-medium rounded-lg text-sm px-4 py-2 text-center hidden md:flex w-max outline outline-1 outline-primary hover:opacity-70">Sign In</button>
           </SignInButton>
           <SignUpButton mode='modal'>
             <button type="button" className="text-neutral-1 bg-primary hover:opacity-90 focus:ring-4 focus:outline-none focus:ring-accent-7 font-medium rounded-lg text-sm px-4 py-2 text-center hidden md:flex">Sign Up</button>
           </SignUpButton>
-          <button data-collapse-toggle="navbar-cta" onClick={handleNavPress} type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 border" aria-controls="navbar-cta" aria-expanded="false">
+          <button data-collapse-toggle="navbar-cta" onClick={handleNavPress} type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 border" aria-controls="navbar-cta" aria-expanded={showNav} aria-label='Toggle navigation menu'>
             <span className="sr-only">Open main menu</span>
             <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
               <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 1h15M1 7h15M1 13h15" />
