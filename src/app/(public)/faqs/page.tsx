3b9da3c438c129f31b185e3b5a14cf968e31435f
gab-cat <75659838+gab-cat@users.noreply.github.com>
2025-01-17 18:51:46 +0000

    refactor: faq page layout and components; add FaqsBody for dynamic FAQ rendering

--- src/app/(public)/faqs/page.tsx
+++ src/app/(public)/faqs/page.tsx
@@ -1,45 +1,29 @@
-import {
-  Accordion,
-  AccordionContent,
-  AccordionItem,
-  AccordionTrigger,
-} from "@/components/ui/accordion";
-import { Button } from "@/components/ui/button";
-import { FAQS_CONTENT, FAQS_DETAILS } from '@/constants';
+'use client';
+
+import { AnimatePresence, motion } from 'framer-motion';
+import FaqsBody from '@/components/public/faqs-body';
+import { FAQS_CONTENT } from '@/constants';
 
 const Page = () => {
   return (
-    <div className="min-h-[75vh] flex justify-center" style={{ fontFamily: 'var(--font-inter-sans)' }}>
-      <div className="w-full max-w-4xl px-4 md:px-6">
-        <div className="place-self-center text-5xl py-4">
-          {FAQS_CONTENT.title}
+    <AnimatePresence>
+      <motion.div 
+        initial={{ opacity: 0, y: 20 }}
+        animate={{ opacity: 1, y: 0 }}
+        exit={{ opacity: 0, y: 20 }}
+        transition={{ duration: 0.3 }}
+        className="flex min-h-[75vh] justify-center" style={{ fontFamily: 'var(--font-inter-sans)' }}>
+        <div className="w-full max-w-4xl px-4 md:px-6">
+          <div className="place-self-center py-4 pt-16 text-5xl tracking-tighter md:text-8xl">
+            {FAQS_CONTENT.title}
+          </div>
+          <div className="w-5/6 place-self-center text-sm">
+            {FAQS_CONTENT.description}
+          </div>
+          <FaqsBody />
         </div>
-        <div className="place-self-center w-5/6 text-sm">
-          {FAQS_CONTENT.description}
-        </div>
-        <div className="py-4 flex justify-center space-x-2">
-          <Button variant="outline" color="blue" className="rounded-full">Orders</Button>
-          <Button variant="outline" color="blue" className="rounded-full">Payments</Button>
-          <Button variant="outline" color="blue" className="rounded-full">Products</Button>
-          <Button variant="outline" color="blue" className="rounded-full">Returns & Refunds</Button>
-          <Button variant="outline" color="blue" className="rounded-full">Contact & Support</Button>
-        </div>
-        {/* Rendering a dynamic list of FAQs and their corresponding answers */}
-        <Accordion 
-          type="single" 
-          collapsible 
-          className="py-4"
-          aria-label="Frequently Asked Questions"
-        >
-          {FAQS_DETAILS.map((faq, index) => (
-            <AccordionItem key={index} value={`item-${index}`}>
-              <AccordionTrigger>{faq.question}</AccordionTrigger>
-              <AccordionContent>{faq.answer}</AccordionContent>
-            </AccordionItem>
-          ))}
-        </Accordion>
-      </div>
-    </div>
+      </motion.div>
+    </AnimatePresence>
   );
 };
 
