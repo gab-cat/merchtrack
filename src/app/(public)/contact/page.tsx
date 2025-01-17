924e62ae4e17e43ebe18d160d38aec550b5713a4
Reibanns <reivenlee@gmail.com>
2025-01-18 05:47:05 +0800

    feat(render-contact-us-page): add text area component and implemented some suggestions

--- src/app/(public)/contact/page.tsx
+++ src/app/(public)/contact/page.tsx
@@ -1,19 +1,21 @@
-'use client'
+'use client';
 
-import { Button } from "@/components/ui/button"
-import { Input } from "@/components/ui/input"
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { TextArea } from "@/components/ui/text-area";
 
 const ContactPage = () => {
   return (
+<div aria-label="Contact form section">
     <div className="container mx-auto px-4 py-8 md:py-8 max-w-2xl">
       <div className="space-y-6">
-        <div className="space-y-2 text-center">
+        <div className="space-y-2 text-center" id="contact-form-header">
           <h1 className="text-3xl md:text-5xl font-bold text-neutral-8">Contact Us</h1>
           <p className="text-muted-foreground text-neutral-6">
             Got a technical issue? Want to send feedback about a beta feature? Need details about our Business plan? Let us know.
           </p>
         </div>
-        <form className="space-y-4">
+        <form className="space-y-4" aria-labelledby="contact-form-header">
           <div className="space-y-2">
             <label htmlFor="email" className="block font-medium">
               Your email
@@ -39,14 +41,15 @@ const ContactPage = () => {
             <label htmlFor="message" className="block font-medium ">
               Message
             </label>
-            <textarea id="message" rows={10} className="flex w-full rounded-md border border-input bg-transparent px-3 py-2 text-base shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 md:text-sm" placeholder="Write your message here..."/>
+            <TextArea id="message" placeholder="Write your message here..."></TextArea>
           </div>
           <Button className="w-full sm:w-auto text-neutral-1">Send message</Button>
         </form>
       </div>
     </div>
-  )
-}
+</div>
+  );
+};
 
-export default ContactPage
+export default ContactPage;
 
